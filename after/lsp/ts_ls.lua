9a18eb7e40aa4809d9323c269317e694e391f405
hacxy <hacxy.js@outlook.com>
2025-12-31 10:11:26 +0800

    update eslint lsp config

--- after/lsp/ts_ls.lua
+++ after/lsp/ts_ls.lua
@@ -1,44 +1,4 @@
 return {
-  filetypes = {
-    'javascript',
-    'javascriptreact',
-    'javascript.jsx',
-    'typescript',
-    'typescriptreact',
-    'typescript.tsx',
-    'vue',
-    'html',
-    'markdown',
-    'json',
-    'jsonc',
-    'yaml',
-    'toml',
-    'xml',
-    'gql',
-    'graphql',
-    'astro',
-    'svelte',
-    'css',
-    'less',
-    'scss',
-    'pcss',
-    'postcss',
-  },
-  settings = {
-    -- Silent the stylistic rules in your IDE, but still auto fix them
-    rulesCustomizations = {
-      { rule = 'style/*', severity = 'on', fixable = true },
-      { rule = 'format/*', severity = 'on', fixable = true },
-      { rule = '*-indent', severity = 'on', fixable = true },
-      { rule = '*-spacing', severity = 'on', fixable = true },
-      { rule = '*-spaces', severity = 'on', fixable = true },
-      { rule = '*-order', severity = 'on', fixable = true },
-      { rule = '*-dangle', severity = 'on', fixable = true },
-      { rule = '*-newline', severity = 'on', fixable = true },
-      { rule = '*quotes', severity = 'on', fixable = true },
-      { rule = '*semi', severity = 'on', fixable = true },
-    },
-  },
   on_attach = function(client, bufnr)
     client.server_capabilities.documentFormattingProvider = false
     client.server_capabilities.documentRangeFormattingProvider = false
